0ea751dba44c909c5e33ed1b732db324975b12db
Alex <tagoperator@gmail.com>
2017-10-13 16:17:07 +0300

    custom menu 13.10.17

--- js/main.js
+++ js/main.js
@@ -2,16 +2,35 @@ $(document).ready(function() {
 
   var c = console.log;
 
+  /* NAVIGATION */
+  //menu - :hover vs :notNover
+  function hoveredUl() {
+    var hov = $(".liHovered");
+    if ($(window).width() > 767) {
+      hov.addClass("hovered");
+    } else {
+      hov.removeClass("hovered");
+    }
+  }
+  hoveredUl();// see $(window).resize();
+
+  $(".ToggleButton").click(function(){
+    $(".mainNav").css("left", 0);
+  });
+
+
+
+  /* SLIDER */
   // slider height
   function caruselHeightDetect(){
     var caruselHeight = $(window).height() * .6;
     $("#mainSliderC .item").css("height", caruselHeight);
-    c("caruselHeight" + caruselHeight);
   };
-  caruselHeightDetect();
+  caruselHeightDetect();// see $(window).resize();
 
   $(window).resize(function(){
     caruselHeightDetect();
+    hoveredUl();
   });
 
 
@@ -35,4 +54,7 @@ $(document).ready(function() {
     $("body, html").animate({scrollTop: 0}, 500);
   });
 
+
+
+  
 });
