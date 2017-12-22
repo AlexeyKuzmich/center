da5d2713328b758c51f84e7ffe063d977f4e7d6d
AlexKuzmich <tagoperator@gmail.com>
2017-12-22 11:38:10 +0200

    22.12.17_1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-
-
- 
-
-
 function sliderHeightDetect() {
   var sliderHeight = $(window).height() * .7,
       sliderItemHeight = $(".owl-mainSlider .item");
@@ -10,101 +5,117 @@ function sliderHeightDetect() {
 };
 
 
-  // adaptive menu
-  function adjustMenu() {
-    ww = document.body.clientWidth + 17;
-    var $directionItem = $(".direction .item");
-    var counter = 0;
+// adaptive menu
+function adjustMenu() {
+  ww = document.body.clientWidth + 17;
+  var $directionItem = $(".direction .item");
+  var counter = 0;
 
-    if (ww < 768) {
+  if (ww < 768) {
 
-      $(".toggleMenu").css("display", "block");
+    $(".toggleMenu").css("display", "block");
 
-      $(".toggleMenu").click(function(e) {
-        e.preventDefault();
-        if ( counter === 0 ) {
-          $(this).css("transform", "rotate(180deg)");
-          $(".nav").addClass("activeMenu");
-          counter = 1;
-        } else {
-          $(this).css("transform", "rotate(0deg)");
-          $(".nav").removeClass("activeMenu");
-          counter = 0;
-        }
-      });
-
-      $(".nav li").unbind('mouseenter mouseleave');
-      $(".nav li a.parent")
-        .unbind('click')
-          .bind('click', function(e) {
-        // Необходимоо привязать к элементу ссылки для предотвращения "всплывания"
-        e.preventDefault();
-        $(this)
-          .parent("li")
-            .toggleClass("hover");
-      });
-
-      $directionItem.each(function() {
-        $(this)
-          .removeClass("largeScreen")
-            .removeAttr("style");
-      });
-
-    } else if ( ww >= 768 ) {
-
-      // topInfo scroll
-      $(window).scroll(function() {
-        var $nav = $("nav");
-        if ( $(this).scrollTop() > 150 ) {
-          $nav.addClass("fixedMenu");
-        } else {
-          $nav.removeClass("fixedMenu");
-        }
-      });
-      
-      $(".toggleMenu").css("display", "none");
-      $(".nav")
-        .show()
-          .css("display", "block");
-
-      $(".nav li a").unbind('click');
-      $(".nav li")
-        .removeClass("hover")
-          .unbind('mouseenter mouseleave')
-            .bind('mouseenter mouseleave', function() {
-        // Необходимо привязать к элементу li для предотвращения запуска события mouseleave при перемещении курсора мыши над подменю
-        $(this).toggleClass('hover');
-      });
-
-      $directionItem.each(function() {
-        $(this).addClass("largeScreen");
-      });
-
-    }
-  }
-
-
-  // EqualHeight
-  function setEqualHeight(columns) {
-    var tallestColumn = 0;
-    columns.each(function() {
-      columns.removeAttr("style");
-      currentHeight = $(this).height();
-      if ( currentHeight > tallestColumn ) {
-        tallestColumn = currentHeight;
+    $(".toggleMenu").click(function(e) {
+      e.preventDefault();
+      if ( counter === 0 ) {
+        $(this).css("transform", "rotate(180deg)");
+        $(".nav").addClass("activeMenu");
+        counter = 1;
+      } else {
+        $(this).css("transform", "rotate(0deg)");
+        $(".nav").removeClass("activeMenu");
+        counter = 0;
       }
     });
-    columns.height( tallestColumn );
-  }
 
+    $(".nav li").unbind('mouseenter mouseleave');
+    $(".nav li a.parent")
+      .unbind('click')
+        .bind('click', function(e) {
+      // Необходимоо привязать к элементу ссылки для предотвращения "всплывания"
+      e.preventDefault();
+      $(this)
+        .parent("li")
+          .toggleClass("hover");
+    });
+
+    $directionItem.each(function() {
+      $(this)
+        .removeClass("largeScreen")
+          .removeAttr("style");
+    });
+
+  } else if ( ww >= 768 ) {
+
+    // topInfo scroll
+    $(window).scroll(function() {
+      var $nav = $("nav");
+      if ( $(this).scrollTop() > 150 ) {
+        $nav.addClass("fixedMenu");
+      } else {
+        $nav.removeClass("fixedMenu");
+      }
+    });
+    
+    $(".toggleMenu").css("display", "none");
+    $(".nav")
+      .show()
+        .css("display", "block");
+
+    $(".nav li a").unbind('click');
+    $(".nav li")
+      .removeClass("hover")
+        .unbind('mouseenter mouseleave')
+          .bind('mouseenter mouseleave', function() {
+      // Необходимо привязать к элементу li для предотвращения запуска события mouseleave при перемещении курсора мыши над подменю
+      $(this).toggleClass('hover');
+    });
+
+    $directionItem.each(function() {
+      $(this).addClass("largeScreen");
+    });
+
+  }
+}
+
+
+// EqualHeight
+function setEqualHeight(columns) {
+  var tallestColumn = 0;
+  columns.each(function() {
+    columns.removeAttr("style");
+    currentHeight = $(this).height();
+    if ( currentHeight > tallestColumn ) {
+      tallestColumn = currentHeight;
+    }
+  });
+  columns.height( tallestColumn );
+}
+
+
+
+// parallaxImage loading depending on screen size
+function parallaxImageDetect() {
+  var ww = $(window).width(),
+      image = "";
+  if (ww <= 360) {
+    image = "images/test/3.jpg";
+  } else if (ww > 360 && ww <= 767) {
+    image = "images/test/2.jpg";
+  } else {
+    image = "images/test/1.jpg";
+  }
+  console.log("imageParallax = " + image);
+  return image;
+}
 
 
 //****************************** ONLOAD ******************************
 $(document).ready(function() {
   $(".greeting").css("opacity", 1);
   $("#preload")
-    .delay(1500)
-      .fadeOut(1500);
+    .delay(15)
+      .fadeOut(15);
 
 
   var /*ww = document.body.clientWidth,*/
@@ -130,7 +141,7 @@ $(document).ready(function() {
     loop: true,
     margin: 0,
     nav: true,
-    autoplay: true,
+    autoplay: false,
     autoplayTimeout: 3000,
     autoplayHoverPause: true,
     items: 1
@@ -187,9 +198,9 @@ $(document).ready(function() {
 
 
 
-
   // parallax
-  $(".parallax-window").parallax({imageSrc: "images/parallax/1.jpg"});
+  $(".parallax-window").parallax({imageSrc: parallaxImageDetect()}); // в parallaxImageDetect() определяестся загружаемое изображение в зависимости от ширины экрана
+
 
 
 
@@ -223,29 +234,12 @@ $(document).ready(function() {
 
 
 
-    // resize
-    $(window).bind("resize orientationchange", function() {
-      /*ww = document.body.clientWidth;*/
-      adjustMenu();
-      sliderHeightDetect();
-      setEqualHeight( $(".largeScreen") );
-      /*testFunc();*/
-    });
-
-
-    // image loading depending on screen size
-    function testFunc() {
-      /*var ww = $(window).width(),*/
-      var $test = $("#test");
-      if (ww <= 767) {
-        $test.css("backgroundImage","url(images/test/3.jpg)")
-      } else if (ww > 767 && ww <= 1199) {
-        $test.css("backgroundImage","url(images/test/2.jpg)")
-      } else {
-        $test.css("backgroundImage","url(images/test/1.jpg)")
-      }
-    }
-    testFunc();
-
+  // resize
+  $(window).on("resize orientationchange", function() {
+    /*ww = document.body.clientWidth;*/
+    adjustMenu();
+    sliderHeightDetect();
+    setEqualHeight( $(".largeScreen") );
+  });
 
 }); //$(document).ready(function()
