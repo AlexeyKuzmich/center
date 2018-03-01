9580e5d7a3a0dfa8e24b251dfac43c94128d9ecb
AlexKuzmich <tagoperator@gmail.com>
2018-03-01 16:31:03 +0200

    01.03.18_3

--- js/main.js
+++ js/main.js
@@ -139,7 +139,6 @@ $(document).ready(function() {
       $("#preload")
         .delay(preload)
           .fadeOut(preload);
-      preload = 0;
   })();
 
 
@@ -352,19 +351,19 @@ $(document).ready(function() {
   });
 
 
-
-    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
-          var form_data = $(this).serialize(); //собераем все данные из формы
-          $.ajax({
-          type: "POST", //Метод отправки
-          url: "send.php", //путь до php фаила отправителя
-          data: form_data,
-          success: function() {
-                 //код в этом блоке выполняется при успешной отправке сообщения
-                 alert("Ваше сообщение отпрвлено!");
-               }
-          });
-    });
+  // feedback
+  $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+        var form_data = $(this).serialize(); //собераем все данные из формы
+        $.ajax({
+        type: "POST", //Метод отправки
+        url: "send.php", //путь до php фаила отправителя
+        data: form_data,
+        success: function() {
+               //код в этом блоке выполняется при успешной отправке сообщения
+               alert("Ваше сообщение отпрвлено!");
+             }
+        });
+  });
 
 
 
